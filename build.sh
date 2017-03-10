f20cf45de936f9286008b6af395be34f129be66e
William Kennedy <william@a9group.net>
2017-03-10 13:17:47 -0800

    build.sh - local

--- build.sh
+++ build.sh
@@ -2,8 +2,8 @@ DATE=$(date +%Y%m%d)
 
 convert background-base.png -font Arial -pointsize 9 -fill red -annotate +10+10 $(date +%Y%m%d) background.png
 
-rm ~/Downloads/Previous.*.dmg
+rm Previous.*.dmg
 
-appdmg previous.appdmg.json ~/Downloads/Previous.$DATE.dmg
+appdmg previous.appdmg.json Previous.$DATE.dmg
 
-open ~/Downloads/Previous.$DATE.dmg
+open Previous.$DATE.dmg
