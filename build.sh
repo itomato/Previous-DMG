6a86995bf2d842c6332fb92bf8fc1cc8f97f2524
william kennedy <me@williams-MacBook-Pro.local>
2021-08-13 23:32:28 -0400

    2.3 r1038

--- build.sh
+++ build.sh
@@ -2,8 +2,7 @@ DATE=$(date +%Y%m%d)
 
 convert background-base.png -font Arial -pointsize 9 -fill red -annotate +10+10 $(date +%Y%m%d) background.png
 
-rm Previous.*.dmg
+cp Previous.icns Previous.app/Contents/Resources/Previous.icns 
 
-appdmg previous.appdmg.json Previous.$DATE.dmg
+appdmg previous.appdmg.json Previous.2.3.r1038.$DATE.dmg
 
-open Previous.$DATE.dmg
---
+++ setup.sh
@@ -0,0 +1,2 @@
+brew install npm
+npm install -g appdmg
